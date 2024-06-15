f7423e904f4ce23120bb95920f7b9f135c1145df
SantiagoN <orangesant24@gmail.com>
2024-06-14 20:04:01 -0500

    Social media bar and skills sections were added

--- app.js
+++ app.js
@@ -107,115 +107,115 @@ class NavigationPage {
 
   particlesJS(
     {
-"particles": {
-"number": {
-  "value": 29,
-  "density": {
-    "enable": true,
-    "value_area": 710.2328774690454
-  }
-},
-"color": {
-  "value": "#03dac6"
-},
-"shape": {
-  "type": "circle",
-  "stroke": {
-    "width": 0,
-    "color": "#000000"
-  },
-  "polygon": {
-    "nb_sides": 4
-  },
-  "image": {
-    "src": "img/github.svg",
-    "width": 100,
-    "height": 100
-  }
-},
-"opacity": {
-  "value": 0.24852886943415603,
-  "random": false,
-  "anim": {
-    "enable": false,
-    "speed": 1,
-    "opacity_min": 0.1,
-    "sync": false
-  }
-},
-"size": {
-  "value": 3,
-  "random": true,
-  "anim": {
-    "enable": false,
-    "speed": 12.181158184520175,
-    "size_min": 0.1,
-    "sync": false
-  }
-},
-"line_linked": {
-  "enable": true,
-  "distance": 150,
-  "color": "#ffffff",
-  "opacity": 0.4,
-  "width": 1
-},
-"move": {
-  "enable": true,
-  "speed": 6,
-  "direction": "none",
-  "random": false,
-  "straight": false,
-  "out_mode": "out",
-  "bounce": false,
-  "attract": {
-    "enable": false,
-    "rotateX": 600,
-    "rotateY": 1200
-  }
-}
-},
-"interactivity": {
-"detect_on": "canvas",
-"events": {
-  "onhover": {
-    "enable": true,
-    "mode": "repulse"
-  },
-  "onclick": {
-    "enable": true,
-    "mode": "push"
-  },
-  "resize": true
-},
-"modes": {
-  "grab": {
-    "distance": 400,
-    "line_linked": {
-      "opacity": 1
+      "particles": {
+        "number": {
+          "value": 160,
+          "density": {
+            "enable": true,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": "#faebd7"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "#000000"
+          },
+          "polygon": {
+            "nb_sides": 5
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 1,
+          "random": true,
+          "anim": {
+            "enable": true,
+            "speed": 1,
+            "opacity_min": 0,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 3,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 4,
+            "size_min": 0.3,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": false,
+          "distance": 150,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
+        },
+        "move": {
+          "enable": true,
+          "speed": 1,
+          "direction": "none",
+          "random": true,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 600
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": true,
+            "mode": "repulse"
+          },
+          "onclick": {
+            "enable": true,
+            "mode": "push"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 400,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 250,
+            "size": 0,
+            "duration": 2,
+            "opacity": 0,
+            "speed": 3
+          },
+          "repulse": {
+            "distance": 95.90409590409591,
+            "duration": 0.4
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 2
+          }
+        }
+      },
+      "retina_detect": true
     }
-  },
-  "bubble": {
-    "distance": 400,
-    "size": 40,
-    "duration": 2,
-    "opacity": 8,
-    "speed": 3
-  },
-  "repulse": {
-    "distance": 200,
-    "duration": 0.4
-  },
-  "push": {
-    "particles_nb": 4
-  },
-  "remove": {
-    "particles_nb": 2
-  }
-}
-},
-"retina_detect": true
-}
-)
+  )
   
   new NavigationPage();
